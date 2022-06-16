305b18b5df75968b9ce2feb54147365d2f297c26
Trieu010 <91833984+Trieu010@users.noreply.github.com>
2022-06-16 13:55:38 +0700

    hihi

---
+++ Tuan5_NguyenMinhTrieu/Tuan5_NguyenMinhTrieu/Controllers/TheloaiController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tuan5_NguyenMinhTrieu.Models;
+
+namespace Tuan5_NguyenMinhTrieu.Controllers
+{
+    public class TheloaiController : Controller
+    {
+        // GET: Theloai
+        MyDataDataContext data = new MyDataDataContext();
+        public ActionResult Index()
+        {
+            var all_theloai = from tt in data.TheLoais select tt;
+            return View(all_theloai);
+        }
+    }
+}
