9cc47dce40b4a26045ec3e07bcf78eceecbda463
Trieu010 <91833984+Trieu010@users.noreply.github.com>
2022-06-23 15:48:41 +0700

    1911060289_NguyenMinhTrieu_ChamBai1

--- Tuan5_NguyenMinhTrieu/Tuan5_NguyenMinhTrieu/App_Start/RouteConfig.cs
+++ Tuan5_NguyenMinhTrieu/Tuan5_NguyenMinhTrieu/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Tuan5_NguyenMinhTrieu
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Sach", action = "ListSach", id = UrlParameter.Optional }
+                defaults: new { controller = "NguoiDung", action = "DangNhap", id = UrlParameter.Optional }
             );
         }
     }
